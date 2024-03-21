2d22fd93b7455a2d6ca572f7d3b73b7a8e463dcd
Carolyne Ndunge <carohndunge@gmail.com>
2024-03-21 16:25:05 +0300

    Popular products section

--- src/components/shoe-card.jsx
+++ src/components/shoe-card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ShoeCard = (imgURL, changeBigShoeImage, bigShoeImg) => {
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
   const handleClick = () => {
     if (bigShoeImg !== imgURL.bigShoe) {
       changeBigShoeImage(imgURL.bigShoe);
--- src/sections/popular-products.jsx
+++ src/sections/popular-products.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { products } from "../constants";
 
 const PopularProducts = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p>
+        Experience top notch quality and styles with our sought after
+        selections. Discover a world of comfort, design and value.
+      </p>
+    </div>
+  );
 };
 
 export default PopularProducts;
