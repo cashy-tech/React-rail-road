f34381a2bb2e49d8e57cfbc28aed8de4f82f087a
Carolyne Ndunge <carohndunge@gmail.com>
2024-03-24 18:57:51 +0300

    Review card component

--- src/sections/customer-reviews.jsx
+++ src/sections/customer-reviews.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { reviews } from "../constants";
 import ReviewCard from "../components/review-card";
 
@@ -13,16 +12,17 @@ function CustomerReviews() {
         Hear genuine stories from our satisfied customers about their
         exceptional experiences with us.
       </p>
-      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14"></div>
-      {reviews.map((review) => {
-        <ReviewCard
-          key={review.customerName}
-          imgURL={review.imgURL}
-          customerName={review.customerName}
-          rating={review.rating}
-          feedback={review.feedback}
-        />;
-      })}
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => {
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />;
+        })}
+      </div>
     </section>
   );
 }
