d33bea34ec7fa9e105b6fbf91efbe5386b835a9d
Carolyne Ndunge <carohndunge@gmail.com>
2024-04-03 17:08:07 +0300

    Changes on the customer review section (debugging)

--- src/sections/customer-reviews.jsx
+++ src/sections/customer-reviews.jsx
@@ -13,15 +13,15 @@ function CustomerReviews() {
         exceptional experiences with us.
       </p>
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
-        {reviews.map((review) => {
+        {reviews.map((review) => (
           <ReviewCard
             key={review.customerName}
             imgURL={review.imgURL}
             customerName={review.customerName}
             rating={review.rating}
             feedback={review.feedback}
-          />;
-        })}
+          />
+        ))}
       </div>
     </section>
   );
